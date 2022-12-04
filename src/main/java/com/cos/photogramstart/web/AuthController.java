067eb7df09d1c4a623923775c89cec00dc851f73
kmsy120 <kmsy120@naver.com>
2022-12-05 03:14:40 +0900

    회원가입 전처리 과정 추가(2022/12/05)

--- src/main/java/com/cos/photogramstart/web/AuthController.java
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -1,6 +1,7 @@
 package com.cos.photogramstart.web;
 
 import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.handler.ex.CustomValidationException;
 import com.cos.photogramstart.sevice.AuthService;
 import com.cos.photogramstart.web.dto.auth.SignupDto;
 
@@ -8,8 +9,15 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Controller
@@ -30,10 +38,23 @@ public class AuthController {
     }
 
     @PostMapping("/auth/signup")
-    public String signup(SignupDto signupDto){
-        User user = signupDto.toEntity();
-        User signupUser = authService.signup(user);
-        System.out.println(signupUser);
-        return "auth/signin";
+    public String signup(@Valid SignupDto signupDto, BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            throw new CustomValidationException("유효성 검사 실패함",errorMap);
+        }
+         else{
+            User user = signupDto.toEntity();
+            User signupUser = authService.signup(user);
+            System.out.println(signupUser);
+            return "auth/signin";
+        }
+
+
+
     }
 }
