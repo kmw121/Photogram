63e0688f8ba6f5b0e9db86c229209637c3ae32a1
kmsy120 <kmsy120@naver.com>
2022-12-05 01:34:03 +0900

    회원가입 구현(2022/12/05)

--- src/main/java/com/cos/photogramstart/web/AuthController.java
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -1,7 +1,10 @@
 package com.cos.photogramstart.web;
 
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.sevice.AuthService;
 import com.cos.photogramstart.web.dto.auth.SignupDto;
 
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -10,8 +13,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
+@RequiredArgsConstructor
 public class AuthController {
     private static final Logger log = LoggerFactory.getLogger(AuthController.class);
+
+    private final AuthService authService;
 
 
     @GetMapping("/auth/signin")
@@ -25,7 +31,9 @@ public class AuthController {
 
     @PostMapping("/auth/signup")
     public String signup(SignupDto signupDto){
-        log.info(signupDto.toString());
+        User user = signupDto.toEntity();
+        User signupUser = authService.signup(user);
+        System.out.println(signupUser);
         return "auth/signin";
     }
 }
