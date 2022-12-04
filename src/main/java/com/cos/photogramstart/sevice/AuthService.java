bac1dc8caf3c9e1ad02ae00d2b34b556ed131eb8
kmsy120 <kmsy120@naver.com>
2022-12-05 01:44:38 +0900

    비밀번호 해시화 추가(2022/12/05)

--- src/main/java/com/cos/photogramstart/sevice/AuthService.java
+++ src/main/java/com/cos/photogramstart/sevice/AuthService.java
@@ -3,15 +3,23 @@ package com.cos.photogramstart.sevice;
 import com.cos.photogramstart.domain.user.User;
 import com.cos.photogramstart.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @Transactional
     public User signup(User user){
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        user.setRole("ROLE_USER");
         User userEntity = userRepository.save(user);
         return userEntity;
     }
