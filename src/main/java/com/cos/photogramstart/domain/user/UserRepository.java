98e741b3fe3b79dbd2cb5943de1622bcc0e17283
kmsy120 <kmsy120@naver.com>
2022-12-06 04:35:09 +0900

    int->long으로 수정(2022/12/06)

--- src/main/java/com/cos/photogramstart/domain/user/UserRepository.java
+++ src/main/java/com/cos/photogramstart/domain/user/UserRepository.java
@@ -2,6 +2,6 @@ package com.cos.photogramstart.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Integer> {
+public interface UserRepository extends JpaRepository<User,Long> {
     User findByUsername(String username);
 }
