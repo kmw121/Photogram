266a4ea64eb3e1d2314deed27830d76922e653b6
kmsy120 <kmsy120@naver.com>
2022-12-06 04:36:16 +0900

    userupdate 서비스 구현(2022/12/06)

---
+++ src/main/java/com/cos/photogramstart/sevice/UserService.java
@@ -0,0 +1,31 @@
+package com.cos.photogramstart.sevice;
+
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.domain.user.UserRepository;
+import com.cos.photogramstart.handler.ex.CustomValidationApiException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.function.Supplier;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Transactional
+    public User updateUser(Long id,User user){
+
+        User userEntity = userRepository.findById(id).orElseThrow(() -> {return new CustomValidationApiException("찾을 수 없는 ID입니다");});
+        userEntity.setName(user.getName());
+        userEntity.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userEntity.setBio(user.getBio());
+        userEntity.setWebsite(user.getWebsite());
+        userEntity.setPhone(user.getPhone());
+        userEntity.setGender(user.getGender());
+        return userEntity;
+    }
+}
