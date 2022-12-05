d9e3460e57a78fe2356604161531dfc4a48e8ec1
kmsy120 <kmsy120@naver.com>
2022-12-06 07:21:15 +0900

    subscribe Entity(2022/12/06)

---
+++ src/main/java/com/cos/photogramstart/domain/user/subscribe/Subscribe.java
@@ -0,0 +1,45 @@
+package com.cos.photogramstart.domain.user.subscribe;
+
+import com.cos.photogramstart.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "subscribe_uk",
+                        columnNames = {"fromUserId","toUserId"}
+                )
+        }
+)
+public class Subscribe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(name = "fromUserId")
+    @ManyToOne
+    private User fromUser;
+
+    @JoinColumn(name = "toUserId")
+    @ManyToOne
+    private User toUser;
+
+    private LocalDateTime createDate;
+
+    @PrePersist
+    public void setCreateDate() {
+        this.createDate = LocalDateTime.now();
+    }
+}
+
