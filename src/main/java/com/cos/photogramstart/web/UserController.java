bb722c138b0ddad2e31fedf49feb5805f12eaf16
kmsy120 <kmsy120@naver.com>
2022-12-08 11:03:53 +0900

    프로필화면 컨트롤러 수정(2022/12/08)

--- src/main/java/com/cos/photogramstart/web/UserController.java
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -1,17 +1,27 @@
 package com.cos.photogramstart.web;
 
 import com.cos.photogramstart.config.auth.PrincipalDetails;
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.sevice.UserService;
+import com.cos.photogramstart.web.dto.user.UserProfileDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class UserController {
 
-    @GetMapping("/user/{id}")
-    public String profile(@PathVariable int id){
+    private final UserService userService;
+
+    @GetMapping("/user/{pageUserId}")
+    public String profile(@PathVariable Long pageUserId, Model model,@AuthenticationPrincipal PrincipalDetails principalDetails){
+        UserProfileDTO dto = userService.userProfile(pageUserId,principalDetails.getUser().getId());
+
+        model.addAttribute("dto",dto);
         return "user/profile";
     }
 
