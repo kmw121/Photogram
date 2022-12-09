bfebfae2a1ba2b823d212c761b016bdf3a03c6f2
kmsy120 <kmsy120@naver.com>
2022-12-09 11:37:31 +0900

    구독 리스트 받기 기능 추가(2022/12/09)

--- src/main/java/com/cos/photogramstart/sevice/SubscribeService.java
+++ src/main/java/com/cos/photogramstart/sevice/SubscribeService.java
@@ -2,15 +2,22 @@ package com.cos.photogramstart.sevice;
 
 import com.cos.photogramstart.domain.subscribe.SubscribeRepository;
 import com.cos.photogramstart.handler.ex.CustomApiException;
+import com.cos.photogramstart.web.dto.subscribe.SubscribeDto;
 import lombok.RequiredArgsConstructor;
+import org.qlrm.mapper.JpaResultMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class SubscribeService {
 
     private final SubscribeRepository subscribeRepository;
+    private final EntityManager em;
     @Transactional
     public void subscribe(Long fromUserId,Long toUserId){
         try {
@@ -24,5 +31,27 @@ public class SubscribeService {
     @Transactional
     public void unsubscribe(Long fromUserId,Long toUserId){
         subscribeRepository.mUnSubscribe(fromUserId,toUserId);
+    }
+
+    @Transactional(readOnly = true)
+    public List<SubscribeDto> subscribeList(Long principalId, Long pageUserId){
+        StringBuffer sb = new StringBuffer();
+
+        sb.append("SELECT u.id, u.username, u.profileImageUrl, ");
+        sb.append("if((SELECT 1 FROM subscribe WHERE fromUserId = ? AND toUserId = u.id),1,0) subscribeState, ");
+        sb.append("if((?=u.id), 1,0) equalUserState ");
+        sb.append("FROM user u INNER JOIN subscribe s ");
+        sb.append("ON u.id = s.toUserId ");
+        sb.append("WHERE s.fromUserId = ?");
+
+        Query query = em.createNativeQuery(sb.toString())
+                .setParameter(1,principalId)
+                .setParameter(2,principalId)
+                .setParameter(3,pageUserId);
+
+        JpaResultMapper result = new JpaResultMapper();
+        List<SubscribeDto> subscribeDtos = result.list(query, SubscribeDto.class);
+
+        return subscribeDtos;
     }
 }
