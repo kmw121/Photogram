7d8337d4a4a7af512c6e8960921b4c8e71c230c7
kmsy120 <kmsy120@naver.com>
2022-12-09 11:39:49 +0900

    구독정보 확인용 dto(2022/12/09)

---
+++ src/main/java/com/cos/photogramstart/web/dto/subscribe/SubscribeDto.java
@@ -0,0 +1,21 @@
+package com.cos.photogramstart.web.dto.subscribe;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigInteger;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SubscribeDto {
+    private BigInteger id;
+    private String username;
+    private String profileImageUrl;
+    private Integer subscribeState;
+    private Integer equalUserState;
+
+}
