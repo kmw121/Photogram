b85c7048dce3464ced0adbfe6ad4084e1dd2034d
kmsy120 <kmsy120@naver.com>
2022-12-09 11:39:34 +0900

    구독상태 및 구독자 수 추가(2022/12/09)

--- src/main/java/com/cos/photogramstart/sevice/UserService.java
+++ src/main/java/com/cos/photogramstart/sevice/UserService.java
@@ -1,5 +1,6 @@
 package com.cos.photogramstart.sevice;
 
+import com.cos.photogramstart.domain.subscribe.SubscribeRepository;
 import com.cos.photogramstart.domain.user.User;
 import com.cos.photogramstart.domain.user.UserRepository;
 import com.cos.photogramstart.handler.ex.CustomException;
@@ -17,6 +18,7 @@ import java.util.function.Supplier;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final SubscribeRepository subscribeRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Transactional(readOnly = true)
@@ -31,6 +33,11 @@ public class UserService {
         dto.setPageOwnerState(pageUserId == principalId);
         dto.setImageCount(userEntity.getImages().size());
 
+        int subscribeState = subscribeRepository.mSubscribeState(principalId, pageUserId);
+        int subscribeCount = subscribeRepository.mSubscribeCount(pageUserId);
+
+        dto.setSubscribeState(subscribeState==1);
+        dto.setSubscribeCount(subscribeCount);
         return dto;
     }
     @Transactional
