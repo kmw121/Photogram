60209be3453bc399044552b97fd8c3200cc806a8
kmsy120 <kmsy120@naver.com>
2022-12-12 02:34:18 +0900

    페이스북 로그인 기능 구현(2022/12/12)

--- src/main/java/com/cos/photogramstart/config/SecurityConfig.java
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.cos.photogramstart.config;
 
+import com.cos.photogramstart.config.oauth.OAuth2DetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,9 +9,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@RequiredArgsConstructor
 @EnableWebSecurity //해당 파일로 시큐리티를 활성화.
 @Configuration //IoC
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final OAuth2DetailsService oAuth2DetailsService;
 
     @Bean
     public BCryptPasswordEncoder encoder() {
@@ -26,6 +31,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/auth/signin")
                 .loginProcessingUrl("/auth/signin")
-                .defaultSuccessUrl("/");
+                .defaultSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(oAuth2DetailsService);
     }
 }
